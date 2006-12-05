e312902e82f20f3211baae8014468ba00a95b546
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:14:38 +0000

    extension info file for eZ publish 3.9

---
+++ ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class superuserInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Super user",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
