91a753845ca634f89db71bce7a83a36aed84aa33
Kristof Coomans <kristof.coomans@telenet.be>
2009-05-09 10:26:13 +0000

    - updated ezinfo file

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class superuserInfo
         return array(
             'Name' => "Super User",
             'Version' => "2.x",
-            'Copyright' => "Copyright (C) 2005-2007 SCK-CEN, 2008 Kristof Coomans",
+            'Copyright' => "Copyright (C) 2005-2007 SCK-CEN, 2008-2009 Kristof Coomans",
             'License' => "GNU General Public License v2.0"
         );
     }
